4b79d1fd303e57ea295809707feccd6166fbd5e2
JusCampin <37031311+JusCampin@users.noreply.github.com>
2022-09-17 16:48:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GoldPanning
+ 
---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+
+Config.oro_1             = 'Panning for gold.'
+Config.oro_2             = 'Panning for gold..'
+Config.oro_3             = 'Panning for gold...'
+Config.oro_encontrado    = 'You found some gold!'
+Config.oro_no_encontrado = 'You haven\'t found anything.'
+Config.oro_no_recoger    = 'You can\'t pan for gold here.'
+
+
+-- Uncomment the following Config to change the language to English and comment the ones above
+
+
+-- Config.oro_1             = 'Searching gold.'
+-- Config.oro_2             = 'Searching gold..'
+-- Config.oro_3             = 'Searching gold...'
+-- Config.oro_encontrado    = 'You found some gold.'
+-- Config.oro_no_encontrado = 'You haven\'t found anything.'
+-- Config.oro_no_recoger    = 'You can\'t catch gold here.'
